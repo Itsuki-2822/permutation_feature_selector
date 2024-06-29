d5611a91cb325728f256ab11e6d30a6cf4a62f1a
Itsuki-2822 <135577168+Itsuki-2822@users.noreply.github.com>
2024-06-29 16:23:02 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -20,9 +20,13 @@ setup(
     ],
     classifiers=[
         'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
     ],
     long_description=long_description,
     long_description_content_type="text/markdown",
+    python_requires='>=3.9, <4',
 )
